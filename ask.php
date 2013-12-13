368899ae7de6018addb90c6ae834e3738671a9d0
saki69 <saikat.lawrence@gmail.com>
2013-12-14 00:08:04 +0600

    adding user interaction by letting them asking questions to experts.

---
+++ ask.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>KrishokBondhu</title>
+
+<link rel="stylesheet" href="css/reset.css" />
+<link rel="stylesheet" href="css/text.css" />
+<link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+<header>
+    <div class="Page-Top">
+    
+        <div class="Logo">
+            <a href="#">
+            <img src="images/logo.png" />
+            </a>
+        </div><!--End .Logo--> 
+        
+        <div class="Top-Right">
+        	<form>
+            	<input type="text" value="Search" />
+                <input type="submit" value="Search" />
+            </form><!--End Search Form-->
+        	<nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                     <li><a href="#">Info Bank</a></li>
+                    <li><a href="#">Sign In</a></li>
+                </ul>                
+            </nav><!--End nav-->
+            <div class="clear"></div>
+        </div><!--End .Top-Right-->                
+    </div><!--End .Page-Top-->
+</header><!--End Header-->
+
+<div class="Content-Wrapper">
+	<aside style="width:330px; border:1px solid #80bb01;">
+    	<h3 style="background:#80bb01">Your Issues</h3>
+        <div class="notifications">
+        	<a href="#">See All</a>
+            <div class="clear"></div>
+            <ul>
+            	<div class="notification-unit">
+                	<li><strong>Q:</strong> What to to do in this case?</li><br />
+                    <li><strong>A:</strong> Use KMNO3</li>
+                </div>
+                <div class="notification-unit">
+                	<li><strong>Q:</strong> What to to do in this case?</li><br />
+                    <img src="images/diseases/disease1.jpg" width="300px" /><br /><br />
+                    <li><strong>A:</strong> Use KMNO3</li>
+                </div>
+            </ul>
+        </div>
+    </aside><!--End: Aside-->
+    <div class="Main-Conent" style="width:602px;">
+    	<a href="#" class="notify">
+        	<h6>0<img src="images/notify.png" height="15" style="margin-left:10px;" /></h6>
+            Notification(s)
+        </a><!--End: Notify-->
+    	<h1>Ask your expert</h1>
+        <div class="Content-View">        	
+        	<section class="view" style="border:none;background:#eee;">
+            	<div class="ask-expert">
+                	<div style="text-align:right; font-size:16px; color:#80bb01">13 July, 2013</div>
+                	<form>
+                    	<label>Your Question</label><br /><br />
+                    	<textarea></textarea><br /><br />
+                        <label>Upload Picture</label><br /><br />
+                        <input type="submit" value="Ask" />
+                    </form>
+                </div>
+            </section><!-- End .View-->
+            <div class="clear"></div>
+        </div><!-- .Content-View-->
+    </div><!-- .Main-Content-->
+    <div class="clear"></div>
+</div><!--End .Content-Wrapper-->
+
+<footer>
+	&copy; 2013. All rights reserved.
+</footer><!--End Footer-->
+</body>
+</html>
